3b6e0b6c6d8e26ab553fab4477b98533dfd3f886
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-03-16 14:48:00 +0800

    Added comments on 3 .py

--- Week2/3 .py
+++ Week2/3 .py
@@ -3,9 +3,12 @@
 # of both numbers.
 
 def LCM(num1, num2):
+    # To keep track of each multiplicative
     oldnum1 = num1
     oldnum2 = num2
     
+    # LCM of two numbers should be equal to each other
+    # While theyre not equal, iteratively check their multiplacitive
     while (num1 != num2):
         while (num2 > num1):
             num1 += oldnum1
@@ -14,8 +17,8 @@ def LCM(num1, num2):
 
     return num1 # Or return num2
 
+# GCD can be define by LCM, so just reuse the LCM method
 def GCD(num1, num2):
-
     return int(num1*num2/LCM(num1, num2))
 
 
