7905c2774b62c35ab0c5b9c540fe3f124856c9ba
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-05-26 18:19:10 +0800

    Added a new project

---
+++ MSTVisualization/main.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms import tree
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("MST Algorithms")
+        self.geometry("800x600")
+
+        self.graph = nx.Graph()
+
+        self.edge_entries = [tk.Entry(self) for _ in range(3)]
+        tk.Label(self, text="Edges (u, v, w):").grid(column=0, row=0)
+        for i, entry in enumerate(self.edge_entries):
+            entry.grid(column=i+1, row=0)
+        tk.Button(self, text="Add Edge", command=self.add_edge).grid(column=4, row=0)
+
+        tk.Button(self, text="Calculate Kruskal's MST", command=self.calculate_kruskal).grid(column=0, row=1)
+        tk.Button(self, text="Calculate Prim's MST", command=self.calculate_prim).grid(column=1, row=1)
+
+        self.figure = plt.Figure(figsize=(5, 5), dpi=100)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self) 
+        self.canvas.get_tk_widget().grid(column=0, row=2, columnspan=5)
+
+    def add_edge(self):
+        u, v, w = [entry.get() for entry in self.edge_entries]
+        self.graph.add_edge(u, v, weight=float(w))
+        self.plot_graph(self.graph)
+
+    def calculate_mst(self, algorithm):
+        self.graph.clear()
+        edges_copy = self.edges.copy()  # make a copy of the edges to restore later
+        if algorithm == "kruskal":
+            mst = tree.minimum_spanning_edges(self.graph, algorithm='kruskal', data=False)
+        elif algorithm == "prim":
+            mst = tree.minimum_spanning_edges(self.graph, algorithm='prim', data=False)
+        mst_graph = nx.Graph(mst)
+        self.plot_graph(mst_graph)
+        self.graph.add_weighted_edges_from(edges_copy)  # restore the original edges
+
+    def calculate_kruskal(self):
+        self.calculate_mst("kruskal")
+
+    def calculate_prim(self):
+        self.calculate_mst("prim")
+
+    def plot_graph(self, graph):
+        self.figure.clear()
+        pos = nx.spring_layout(graph)
+        nx.draw(graph, pos, with_labels=True, ax=self.figure.add_subplot(111))
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+        self.canvas.draw()
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
