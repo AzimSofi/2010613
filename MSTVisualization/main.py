80307ba9183b51559bedcdac3ed7ad2300873851
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-06-02 10:52:58 +0800

    Update main.py
    Added new feature
    Made GUI looks better

--- MSTVisualization/main.py
+++ MSTVisualization/main.py
@@ -9,59 +9,66 @@ class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("MST Algorithms")
-        self.geometry("1280x720")
-
+        self.geometry("900x850")
+        self.minsize(900, 850)  # Set minimum window size to prevent hiding buttons
         self.graph = nx.Graph()
-
-        self.edge_entries = [tk.Entry(self) for _ in range(3)]
-        tk.Label(self, text="Edges (Node1, Node2, Weight):").grid(column=0, row=0)
+        
+        # Edge input frame
+        edge_frame = tk.Frame(self)
+        edge_frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        tk.Label(edge_frame, text="Edges: ").grid(column=0, row=0)
+        
+        placeholders = ["Node 1", "Node 2", "Weight"]
+        self.edge_entries = [self.create_placeholder_entry(edge_frame, text) for text in placeholders]
         for i, entry in enumerate(self.edge_entries):
             entry.grid(column=i+1, row=0)
-        tk.Button(self, text="Add Edge", command=self.add_edge).grid(column=4, row=0)
+        tk.Button(edge_frame, text="Add Edge", command=self.add_edge).grid(column=4, row=0)
 
-        tk.Button(self, text="Calculate Kruskal's MST",
-                   command=self.calculate_kruskal).grid(column=0, row=2)
-        tk.Button(self, text="Calculate Prim's MST",
-                   command=self.calculate_prim).grid(column=1, row=2)
-
-        tk.Button(self, text="Generate a random graph",
-                   command=self.generate_random_graph_letter).grid(column=0, row=1)
+        # Random graph generation frame
+        random_frame = tk.Frame(self)
+        random_frame.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        tk.Button(random_frame, text="Generate a random graph", command=self.generate_random_graph_letter).grid(column=0, row=0)
+        tk.Button(random_frame, text="Generate a random Japanese route", command=self.generate_random_graph_japanese).grid(column=1, row=0)
         
-        tk.Button(self, text="Generate a random Japanese route",
-                    command=self.generate_random_graph_japanese).grid(column=1, row=1)
+        # MST calculation and graph connectivity indicator frame
+        mst_and_indicator_frame = tk.Frame(self)
+        mst_and_indicator_frame.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
+
+        # MST calculation frame
+        mst_frame = tk.Frame(mst_and_indicator_frame)
+        mst_frame.grid(row=0, column=0, sticky="ew")
+        self.mst_kruskal_button = tk.Button(mst_frame, text="Calculate Kruskal's MST", command=self.calculate_kruskal)
+        self.mst_kruskal_button.grid(column=0, row=0)
+        self.mst_prim_button = tk.Button(mst_frame, text="Calculate Prim's MST", command=self.calculate_prim)
+        self.mst_prim_button.grid(column=1, row=0)
+
+        # Graph connectivity indicator
+        self.graph_indicator = tk.StringVar()
+        self.graph_indicator.set("Connected: ❌")
+        self.graph_indicator_label = tk.Label(mst_and_indicator_frame, textvariable=self.graph_indicator)
+        self.graph_indicator_label.grid(row=0, column=1)
         
-        tk.Button(self, text="Reset graph",
-                    command=self.clear_graph).grid(column=0, row=4)
-        # 閉じるボタン
-        tk.Button(self, text="Exit (閉じる)",
-                    command=self.quit).grid(column=1, row=4)
-
-
+        # Graph canvas
         self.figure = plt.Figure(figsize=(8, 8), dpi=100)  # Increased figsize
         self.canvas = FigureCanvasTkAgg(self.figure, master=self) 
-        self.canvas.get_tk_widget().grid(column=0, row=3,
-                                        columnspan=5, sticky="nsew")  # Use sticky="nsew" to allow expansion
+        self.canvas.get_tk_widget().grid(row=4, column=0,  # Change the row to 4
+                                         columnspan=5, sticky="nsew")  # Use sticky="nsew" to allow expansion
+
+        # Control frame
+        control_frame = tk.Frame(self)
+        control_frame.grid(row=5, column=0, padx=10, pady=10)
+        tk.Button(control_frame, text="Reset graph", command=self.clear_graph).grid(row=0, column=0, padx=20)  # Add padx to add padding between buttons
+        tk.Button(control_frame, text="Exit (閉じる)", command=self.quit).grid(row=0, column=1, padx=20)
 
         # Configure grid layout
-        self.grid_rowconfigure(3, weight=1)  # Set higher weight to row 3
+        self.grid_rowconfigure(4, weight=1)  # Set higher weight to row 4
         self.grid_columnconfigure(0, weight=1, minsize=200)  # Set higher weight and minsize to column 0
-
-    def generate_random_graph_letter(self):
-        self.random_graph('letter')
-
-    def generate_random_graph_japanese(self):
-        self.random_graph('japan_city_names')    
-
-    def would_form_cycle(self, G, edge):
-        u, v = edge
-        if u in G.nodes and v in G.nodes:
-            if u in nx.node_connected_component(G, v):
-                return True
-        return False
-
-    def clear_graph(self):
-        self.graph.clear()
-        self.plot_graph(self.graph)
+        control_frame.columnconfigure(0, weight=1)  # Center the buttons by expanding the columns in control_frame
+        control_frame.columnconfigure(1, weight=1)
+        control_frame.columnconfigure(2, weight=1)
+        
+        # Update connectivity indicator
+        self.update_graph_indicator()
 
     # 作成済みグラフ
     def random_graph(self, things_to_random):
@@ -111,13 +118,8 @@ class Application(tk.Tk):
             # if the graph is fully connected, break the loop, else generate a new graph
             if nx.is_connected(self.graph):
                 break
-
         self.plot_graph(self.graph)  # display the random graph
-
-    def add_edge(self):
-        u, v, w = [entry.get() for entry in self.edge_entries]
-        self.graph.add_edge(u, v, weight=float(w))
-        self.plot_graph(self.graph)
+        self.update_graph_indicator()
 
     def calculate_mst(self, algorithm):
         edges_copy = list(self.graph.edges(data=True))  # make a copy of the edges to restore later
@@ -151,6 +153,31 @@ class Application(tk.Tk):
 
         self.graph.clear()
         self.graph.add_edges_from(edges_copy)  # restore the original edges
+        self.update_graph_indicator()
+
+    def clear_graph(self):
+        self.graph.clear()
+        self.plot_graph(self.graph)
+        self.update_graph_indicator()
+    #
+    def add_edge(self):
+        u, v, w = [entry.get() for entry in self.edge_entries]
+        self.graph.add_edge(u, v, weight=float(w))
+        self.plot_graph(self.graph)
+        self.update_graph_indicator()  # Update the graph indicator and buttons
+       
+    def generate_random_graph_letter(self):
+        self.random_graph('letter')
+
+    def generate_random_graph_japanese(self):
+        self.random_graph('japan_city_names')    
+
+    def would_form_cycle(self, G, edge):
+        u, v = edge
+        if u in G.nodes and v in G.nodes:
+            if u in nx.node_connected_component(G, v):
+                return True
+        return False
 
     def calculate_kruskal(self):
         self.calculate_mst("kruskal")
@@ -176,6 +203,42 @@ class Application(tk.Tk):
         nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_size=20, ax=ax)
         
         self.canvas.draw()
+
+    def update_graph_indicator(self):
+        if nx.is_empty(self.graph) or not nx.is_connected(self.graph):
+            self.graph_indicator.set("Connected: ❌")
+        else:
+            self.graph_indicator.set("Connected: ✔️")
+
+        self.enable_disable_buttons()
+
+    def enable_disable_buttons(self):
+        state = tk.NORMAL if self.is_graph_valid() else tk.DISABLED
+        self.mst_kruskal_button.config(state=state)
+        self.mst_prim_button.config(state=state)
+
+    def is_graph_valid(self):
+        return not nx.is_empty(self.graph) and nx.is_connected(self.graph)
+
+    def create_placeholder_entry(self, parent, placeholder):
+        entry = tk.Entry(parent)
+        entry.insert(0, placeholder)
+        entry['fg'] = 'gray'
+        entry.bind("<FocusIn>", lambda event: self.clear_placeholder_text(event, placeholder))
+        entry.bind("<FocusOut>", lambda event: self.add_placeholder_text(event, placeholder))
+        return entry
+
+    def clear_placeholder_text(self, event, placeholder):
+        widget = event.widget
+        if widget.get() == placeholder:
+            widget.delete(0, tk.END)
+            widget['fg'] = 'black'
+
+    def add_placeholder_text(self, event, placeholder):
+        widget = event.widget
+        if widget.get() == '':
+            widget.insert(0, placeholder)
+            widget['fg'] = 'gray'
 
 # スクリプトが直接実行された場合にアプリケーションを実行します
 if __name__ == "__main__":
